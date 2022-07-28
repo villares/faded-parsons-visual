6edc9087fab89e40c85943e2b7566eda2176069c
Pamela Fox <pamela.fox@gmail.com>
2022-07-28 16:19:34 -0700

    more functions exercises

---
+++ parsons_probs/calculate_dog_age.py
@@ -0,0 +1,10 @@
+def calculate_dog_age(human_age):
+    """ Returns a dog's age based on a conversion rate of
+    1 human year to 7 dog years.
+    >>> calculate_dog_age(1)
+    7
+    >>> calculate_dog_age(0.5)
+    3.5
+    >>> calculate_dog_age(12)
+    84
+    """
---
+++ parsons_probs/tell_fortune.py
@@ -0,0 +1,11 @@
+def tell_fortune(job, place, partner):
+    """ Returns a fortune of the form:
+     'You will be a <job> in <place> living with <partner>.'
+
+    >>> tell_fortune('bball player', 'Spain', 'Sonic the Hedgehog')
+    'You will be a bball player in Spain living with Sonic the Hedgehog'
+    >>> tell_fortune('farmer', 'Kansas', 'C3PO')
+    'You will be a farmer in Kansas living with C3PO'
+    >>> tell_fortune('Elvis Impersonator', 'Russia', 'Karl the Fog')
+    'You will be a Elvis Impersonator in Russia living with Karl the Fog'
+    """
