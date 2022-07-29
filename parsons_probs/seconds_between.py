a06391ed621b3bb090670f2548343e4152f554aa
Pamela Fox <pamela.fox@gmail.com>
2022-07-29 09:50:14 -0700

    Seconds between

---
+++ parsons_probs/seconds_between.py
@@ -0,0 +1,8 @@
+def seconds_between(year1, year2):
+    """ Returns the number of seconds between two years.
+
+    >>> seconds_between(1984, 1985)
+    31536000
+    >>> seconds_between(1984, 2000)
+    504576000
+    """
