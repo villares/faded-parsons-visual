d333074ea8ec35e94580787537c209761046f158
Pamela Fox <pamela.fox@gmail.com>
2022-07-29 16:45:38 -0700

    Conditionals probs

---
+++ parsons_probs/greater_num.py
@@ -0,0 +1,10 @@
+def greater_num(num1, num2):
+    """ Returns whichever number is highest of the two supplied numbers.
+
+    >>> greater_num(45, 10)
+    45
+    >>> greater_num(-1, 30)
+    30
+    >>> greater_num(20, 20)
+    20
+    """
