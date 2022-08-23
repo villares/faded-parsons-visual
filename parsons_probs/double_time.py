2d565efd6e3d6821269f09308dd7da28a025f8ad
Pamela Fox <pamela.fox@gmail.com>
2022-08-23 13:47:04 -0700

    Add list mutation

---
+++ parsons_probs/double_time.py
@@ -0,0 +1,11 @@
+def double_time(sequence):
+    """
+    >>> nums = [1, 2, 3]
+    >>> double_time(nums)
+    >>> nums
+    [2, 4, 6]
+    >>> nums2 = [-1, -4]
+    >>> double_time(nums2)
+    >>> nums2
+    [-2, -8]
+    """
