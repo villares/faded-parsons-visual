9e1c4c2177146f3bd5c16214622d9dddbf75a3bf
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-12-13 11:16:25 -0300

    Update updade_index.py

--- updade_index.py
+++ updade_index.py
@@ -14,9 +14,9 @@ index_html = 'index.html'
 puzzle_list = []
 for yaml_file in puzzle_files:
     with open(yaml_file) as file:
-        name = [li[14:].strip() for li in file.readlines() if li.startswith('problem_name: ')]
-    name = name[0] if name else '-no name-'
-    puzzle_list.append((yaml_file.stem, name))  # nome do arquivo sem extensão, nome dentro do yaml
+        title = [li[14:].strip() for li in file.readlines() if li.startswith('problem_name: ')]
+    title = title[0] if title else '-no title-'
+    puzzle_list.append((yaml_file.stem, title))  # nome do arquivo sem extensão, nome dentro do yaml
     
 with open(index_html) as index_file:
     html = index_file.read()
@@ -27,8 +27,8 @@ ul = div.find('ul')
 ul.decompose()  # destroi/apaga a lista original
 
 new_list = ''
-for file, name in puzzle_list:
-    new_list += f'<li><a href="problem.html?name={file}">{name}</a></li>\n'
+for file_name, title in puzzle_list:
+    new_list += f'<li><a href="problem.html?name={file_name}">{title}</a></li>\n'
 div.append(BeautifulSoup('<ul>\n' + new_list + '\n</ul>', 'html.parser'))
 
 with open(index_html, 'w') as index_file:
