aceffd64aebfd814a706da0ae62be0030490ea6f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-12-14 17:05:11 -0300

    update_index.py now understands categories!

--- update_index.py
+++ update_index.py
@@ -1,31 +1,41 @@
 from pathlib import Path
 from bs4 import BeautifulSoup
-
+from collections import defaultdict
+ 
+puzzles_cat = defaultdict(list)  # puzzle category: [puzzle0, ...]
 # This script should run from the same directory as the index.html file
 index_html = 'index.html'
 parsons_probs = Path.cwd() / 'parsons_probs'      # the puzzles dir
-puzzle_files = [f for f in parsons_probs.iterdir()
-                if f.suffix.lower() == '.yaml']
+# Get a list of the parsons_probs subdir contents
+subdir_contents = list(parsons_probs.iterdir())
+# Get only the yaml files
+puzzle_files = [f for f in subdir_contents if f.suffix.lower() == '.yaml']
 
-puzzle_list = []   # (file-name-without-yaml-extension, problem-title)
 for yaml_file in sorted(puzzle_files):
     with open(yaml_file) as file:
-        title = [li[14:].strip() for li in file.readlines() if li.startswith('problem_name: ')]
-    title = title[0] if title else '-no title-'
-    puzzle_list.append((yaml_file.stem, title))  # nome do arquivo sem extensão, nome dentro do yaml
-    
+        lines = list(file.readlines())
+        # nome/título vem do yaml
+        tl = [line[14:].strip() for line in lines if line.startswith('problem_name: ')]
+        title = tl[0] if tl else 'Unnamed Problem'
+        # subtítulo/descrição curto vem do yaml
+        stl = [line[18:].strip() for line in lines if line.startswith('problem_subtitle: ')]
+        sub_title = stl[0] if stl else ''
+        cl = [line[18:].strip() for line in lines if line.startswith('problem_category: ')]
+        category = cl[0] if cl else ''
+    puzzles_cat[category].append((yaml_file.stem, title, sub_title))#
+ 
 with open(index_html) as index_file:
     html = index_file.read()
 
 soup = BeautifulSoup(html, 'lxml')
 div = soup.find('div', {'id': 'problem-list'})  # adicionei essa div pra facilitar
-ul = div.find('ul')
-ul.decompose()  # destroi/apaga a lista original
-
-new_list = ''
-for file_name, title in puzzle_list:
-    new_list += f'<li><a href="problem.html?name={file_name}">{title}</a></li>\n'
-div.append(BeautifulSoup('<ul>\n' + new_list + '\n</ul>', 'html.parser'))
-
+div.clear()
+for cat, puzzle_list in sorted(puzzles_cat.items()):
+    div.append(BeautifulSoup(f'<h5>{cat}</h5>', 'html.parser'))   
+    new_list = ''
+    for file_name, title, sub_title in puzzle_list:
+        new_list += f'<li><a href="problem.html?name={file_name}">{title}</a> {sub_title}</li>\n'
+    div.append(BeautifulSoup('<ul>\n' + new_list + '\n</ul>', 'html.parser'))
+     
 with open(index_html, 'w') as index_file:
     index_file.write(str(soup))
