5b7a8f33b2a6b70483867e6ffc962802c03465cd
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-04-27 14:07:13 -0300

    Update 130_olhos.py

--- parsons_probs/130_olhos.py
+++ parsons_probs/130_olhos.py
@@ -2,20 +2,20 @@ def setup(): #0given
     size(400, 400) #1given
     olho(100, 150, 100) #1given
     olho(250, 250, 200) #1given
-        
+# definição do olho  #0given
 def olho(x, y, tamanho): #0given
-    no_stroke() #1given
-    fill(255)   #1given
-    ellipse(x, y, tamanho, tamanho * 0.40) #1given
-    fill(200, 200, 0) #1given
-    circle(x, y, tamanho * 0.40) #1given
-    fill(0) #1given
-    circle(x, y, tamanho * 0.10) #1given
+    no_stroke() # sem contorno
+    fill(255)   # branco
+    ellipse(x, y, tamanho, tamanho * 0.40) # branco do olho
+    fill(200, 200, 0) # amarelo
+    circle(x, y, tamanho * 0.40) # iris
+    fill(0) # preto (0)
+    circle(x, y, tamanho * 0.10) # pupila
     
 # fim do código do quebra-cabeças - gerando metadados
 file = Path(__file__).stem
 name = 'Função olho'
-subt = 'definindo uma função de desenho e a chamando.'
+subt = 'Definindo uma função de desenho e a chamando.'
 dcat = 'funções'
 desc = (
 f"""<img src="parsons_probs/{file}.png"></br>"""
@@ -33,22 +33,6 @@ def format_source():
             fline = '  ' + lin.strip(' ') # preserves \n
             if fline.strip():  # skip empty lines
                 code_lines += fline
-    
-#     code_lines = """\
-#   def setup(): #0given
-#   size(400, 400) #1given
-#   olho(100, 150, 100) #1given
-#   olho(250, 250, 200) #1given
-#   # definição do olho  #0given  
-#   def olho(x, y, tamanho): #0given
-#   no_stroke()  # sem contorno
-#   fill(255)  # branco
-#   ellipse(x, y, tamanho, tamanho * 0.40)
-#   fill(200, 200, 0) # amarelo
-#   circle(x, y, tamanho * 0.40)  # iris
-#   fill(!BLANK)   # preto (0)
-#   circle(x, y, tamanho * 0.10)  # pupila
-# """
     return code_lines
 
 def exiting():
@@ -60,7 +44,7 @@ problem_name: {name}
 
 problem_subtitle: {subt}
 
-problem_dcatory: {dcat}
+problem_category: {dcat}
 
 problem_description: |
   {desc}
