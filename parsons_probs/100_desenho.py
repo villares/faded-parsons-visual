fc11e5eb26ec6e39f2d2a4a13691b24fb9128fa8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-05-30 15:49:43 -0300

    melhorando o primeiro execício

--- parsons_probs/100_desenho.py
+++ parsons_probs/100_desenho.py
@@ -1,8 +1,8 @@
-def setup():
-    size(400, 400)
-    background(0, 200, 0) # verde
+def setup(): #0given
+    size(400, 400) #1given
+    background(0, 200, 0) # verde 
     rect_mode(CENTER)
-    circle(200, 50, 50)
+    circle(200, 50, 50) #1given
     fill(0)  # preto
     ellipse(200, 150, 100, 50)
     fill(0, 0, 200)  # azul
@@ -12,49 +12,53 @@ def setup():
     fill(255, 0, 0)  # vermelho
     rect(200, 350, 100, 50)
     
-### fim do código do quebra-cabeças - gerando metadados
-def exiting():
-    file = Path(__file__).stem
-    save(file + '.png')
-    print(file)
-    nome = 'Desenho e cor'
-    subt = 'Formas básicas, preenchimento e traço.'
-    categoria = '100 Primeiros passos'
-    instructions = "Organize os blocos para desenhar as figuras."
-
-    descrição = (
-f"""<img src="parsons_probs/{file}.png"></br>"""
-f"""<code>{subt}</code> {instructions}</br>"""
+# END OF PUZZLE - GENERATING METADATA
+file = Path(__file__).stem
+name = 'Desenho, formas e cores'
+subt = 'Formas básicas, cores de preenchimento e traço'
+dcat = '100 Primeiros passos'
+desc = (
+f"""<h6>{name}</h6></br>"""
+f"""<img src="parsons_probs/{file}.gif"></br>"""
+f"""<code>{subt}</code></br>"""
+f"""Organize os blocos para desenhar as figuras."""
+f"""Preste atenção nas cores dos elementos e das linhas de contorno."""
     )
+
+def format_source():
+    with open(__file__) as f:
+        code_lines = ''
+        for lin in f.readlines():
+            if lin.startswith('# END'):
+                break
+            elif '!REMOVE' in lin:
+                continue
+            elif '!BLANK' in lin:
+                lin.lstrip('#')  # to add comments with !BLANK to puzzle
+            lin = lin.strip(' ') # this preserves \n
+            if lin.strip(): # skip empty lines as .strip() removes \n
+                code_lines += '  ' + lin
+    return code_lines
+
+def exiting():
+    #save(file + '.png')  # save image result
+    print(file)
+    code_lines = format_source()
     yaml = f"""\
-problem_name: {nome} 
+problem_name: {name} 
 
 problem_subtitle: {subt}
 
-problem_category: {categoria}
+problem_category: {dcat}
 
 problem_description: |
-  {descrição}
+  {desc}
    
 code_lines: |
-    def setup(): #0given
-    size(400, 400) #1given
-    background(0, 200, 0) # verde
-    rect_mode(CENTER)
-    circle(200, 50, 50)
-    fill(0)  # preto
-    ellipse(200, 150, 100, 50)
-    fill(0, 0, 200)  # azul
-    no_stroke() # desliga traço
-    square(200, 250, 50)
-    stroke(255, 255, 0) # amarelo
-    fill(255, 0, 0)  # vermelho
-    rect(200, 350, 100, 50)
+{code_lines}
   
 test_fn: setup
 """
     with open(file + '.yaml', 'w') as f:
         f.write(yaml)
-    
-    
     
