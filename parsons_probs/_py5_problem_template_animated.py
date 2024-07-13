71cf5bc779632d3b2a87365dd3cdf3fbc7b81e19
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-07-13 19:30:40 -0300

    templates

--- parsons_probs/_py5_problem_template_animated.py
+++ parsons_probs/_py5_problem_template_animated.py
@@ -1,19 +1,35 @@
+import py5_tools  #!REMOVE
 
 # setup: configurações iniciais #0given
 def setup(): #0given
     size(400, 400) #1given
-    square(200, 250, 50) #1given
+    #!REMOVE code to capture animation
+#     py5_tools.animated_gif(file + '.gif', #!REMOVE
+#                            duration=0.2,  #!REMOVE 
+#                            frame_numbers=range(10,140,2)) #!REMOVE
+# draw: código que repete #0given    
+def draw(): #0given
+    fill(255) # preenchimento branco
+    no_stroke() # desliga traço
+    circle(mouse_x, mouse_y, 50) #1given
+    fill(0, 0, 255) # preenchimento azul
+    stroke_weight(3) # traço mais espesso
+    stroke(255, 255, 0) # traço amarelo
+    square(200, 250, 50)
     
-# END OF PUZZLE - GENERATING METADAA
+# END OF PUZZLE - GENERATING METADATA
 file = Path(__file__).stem
-name = 'Exemplo estático'
-subt = 'Quadradinho'
+name = 'Interação com o mouse'
+subt = 'Preenchimento, traço e interação usando draw()'
 dcat = '100 - primeiros passos'
 desc = (
 f"""<h6>{name}</h6></br>"""
-f"""<img src="parsons_probs/{file}.png"></br>"""
+f"""<img src="parsons_probs/{file}.gif"></br>"""
 f"""<code>{subt}</code></br>"""
 f"""Organize os blocos para fazer um sketch como este."""
+f"""Note que o círculo, com a posição controlada pela ponta do mouse"""
+f""" não tem traço de contorno, e que o quadrado, sempre redesenhado"""
+f""" no mesmo lugar, tem um traço de contorno amarelo mais espesso."""
     )
 
 def format_source():
@@ -32,7 +48,7 @@ def format_source():
     return code_lines
 
 def exiting():
-    save(file + '.png')  # save image result
+    #save(file + '.png')  # save image result
     print(file)
     code_lines = format_source()
     yaml = f"""\
@@ -52,3 +68,4 @@ test_fn: setup
 """
     with open(file + '.yaml', 'w') as f:
         f.write(yaml)
+    
