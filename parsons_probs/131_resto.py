f06f4cc1c209ce24f134d0e2af734866a96842d5
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-09-08 16:25:25 -0300

    ajuste no resto da divisão e xadrez

--- parsons_probs/131_resto.py
+++ parsons_probs/131_resto.py
@@ -1,10 +1,12 @@
-def setup():
-    size(400, 400)
+def setup(): #0given
+    size(400, 400) #1given
+    background(120, 120, 240) #1given
+    stroke_weight(2)
     for i in range(2, 18):
         if i % 2 == 0:
-            stroke(255)
+            stroke(255) # white
         else:
-            stroke(0)
+            stroke(0) # black
         y = 10 + i * 20
         line(50, y, width - 50, y)
         
@@ -17,8 +19,10 @@ def exiting():
     nome = 'Pares e ímpares'
     subt = 'Usando o resto da divisão para alternar cores.'
     categoria = '130 - laços de repetição'
-    instructions = "Organize os blocos para desenhar as linhas corretamente."
-    
+    instructions = (
+"Organize os blocos para desenhar as linhas corretamente. "
+"Note como a primeira linha é branca."
+)    
     descrição = (
 f"""<img src="parsons_probs/{file}.png"></br>"""
 f"""<code>{subt}</code> {instructions}</br>"""
