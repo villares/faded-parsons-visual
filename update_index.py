4ad8dfed4d24835d23a4b915b8dccaa582b449c8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-09-07 22:17:22 -0300

    Update update_index.py

--- update_index.py
+++ update_index.py
@@ -17,6 +17,11 @@ subdir_contents = list(parsons_probs.iterdir())
 # Get only the yaml files
 puzzle_files = [f for f in subdir_contents if f.suffix.lower() == '.yaml']
 
+def get_img_file(li):
+    start = li.find('img src="parsons_probs/') + 23
+    end = li.find('"', start)
+    return li[start:end]
+
 for yaml_file in sorted(puzzle_files):
     with open(yaml_file) as file:
         lines = list(file.readlines())
@@ -28,8 +33,10 @@ for yaml_file in sorted(puzzle_files):
         sub_title = stl[0] if stl else ''
         cl = [line[18:].strip() for line in lines if line.startswith('problem_category: ')]
         category = cl[0].lower() if cl else 'geral'
-    puzzles_cat[category].append((yaml_file.stem, title, sub_title))
-    print(category, yaml_file.stem, title, sub_title)
+        il = [get_img_file(line) for line in lines if 'img src=' in line]
+        img_file = il[0] if il else ''
+    puzzles_cat[category].append((yaml_file.stem, title, sub_title, img_file))
+    print(category, yaml_file.stem, img_file, title, sub_title, sep=' | ')
  
 with open(index_html) as index_file:
     html = index_file.read()
@@ -40,8 +47,10 @@ div.clear()
 for cat, puzzle_list in sorted(puzzles_cat.items()):
     div.append(BeautifulSoup(f'<h5>{cat}</h5>', 'html.parser'))   
     new_list = ''
-    for file_name, title, sub_title in puzzle_list:
-        new_list += f'<li><a href="problem.html?name={file_name}">{title}</a> {sub_title}</li>\n'
+    for file_name, title, sub_title, img_file in puzzle_list:
+        new_list += (f'<li><a href="problem.html?name={file_name}">' 
+                     f'<img src="parsons_probs/{img_file}" alt="{title}" class="thumbnail">'
+                     f'{title}</a> {sub_title}</li>\n')
     div.append(BeautifulSoup('<ul>\n' + new_list + '\n</ul>', 'html.parser'))
      
 with open(index_html, 'w') as index_file:
